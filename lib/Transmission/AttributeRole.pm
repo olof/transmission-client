15fe7ef2f51cfddf6dce84407466d6a5c1d7e89c
Jan Henning Thorsen <jhthorsen@cpan.org>
2009-09-26 20:01:14 +0200

    moved internal translation methods to AttributeRole

--- lib/Transmission/AttributeRole.pm
+++ lib/Transmission/AttributeRole.pm
@@ -54,6 +54,48 @@ has eager_read => (
     trigger => sub { $_[0]->read_all if($_[1]) },
 );
 
+sub _translateCamel {
+    my $self = shift;
+    my $h    = shift;
+
+    for(keys %$h) {
+        (my $key = $_) =~ s/([A-Z]+)/{ "_" .lc($1) }/ge;
+
+        if(my $tr = $self->can("_translate_$key")) {
+            $h->{$key} = $tr->( delete $h->{$_} );
+        }
+        else {
+            $h->{$key} = delete $h->{$_};
+        }
+
+        if(ref $h->{$key} eq 'HASH') {
+            $self->_translateCamel($h->{$key});
+        }
+    }
+}
+
+sub _translate_status {
+    return 'queued'      if($_[0] == 1);
+    return 'checking'    if($_[0] == 2);
+    return 'downloading' if($_[0] == 4);
+    return 'seeding'     if($_[0] == 8);
+    return 'stopped'     if($_[0] == 16);
+    return $_[0];
+}
+
+sub _translate_keys {
+    my $self = shift;
+    my %args = @_;
+
+    for my $orig (keys %args) {
+        my $new = $orig;
+        $new =~ tr/_/-/;
+        $args{$new} = delete $args{$orig};
+    }
+
+    return %args;
+}
+
 =head1 LICENSE
 
 =head1 AUTHOR
