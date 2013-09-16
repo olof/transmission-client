252e895f2c1ce3ad25b05fdfafbbda784e0bb8b3
Olof Johansson <olof@ethup.se>
2013-09-16 19:26:10 +0200

    Prepare for release 0.0803

--- lib/Transmission/Client.pm
+++ lib/Transmission/Client.pm
@@ -14,7 +14,7 @@ Transmission::Client - Interface to Transmission
 
 =head1 VERSION
 
-0.0802
+0.0803
 
 =head1 DESCRIPTION
 
@@ -91,7 +91,7 @@ use Transmission::Torrent;
 use Transmission::Session;
 use constant RPC_DEBUG => $ENV{'TC_RPC_DEBUG'};
 
-our $VERSION = '0.0802';
+our $VERSION = '0.0803';
 our $SESSION_ID_HEADER_NAME = 'X-Transmission-Session-Id';
 my $JSON = JSON::Any->new;
 
