b18c01e43b17fc395f7a21fc7666f8adc5a3e0a6
Olof Johansson <olof@ethup.se>
2019-11-28 21:24:50 +0100

    Bump version to 0.0806

--- lib/Transmission/Client.pm
+++ lib/Transmission/Client.pm
@@ -14,7 +14,7 @@ Transmission::Client - Interface to Transmission
 
 =head1 VERSION
 
-0.0805
+0.0806
 
 =head1 DESCRIPTION
 
@@ -99,7 +99,7 @@ use Transmission::Torrent;
 use Transmission::Session;
 use constant RPC_DEBUG => $ENV{'TC_RPC_DEBUG'};
 
-our $VERSION = '0.0805';
+our $VERSION = '0.0806';
 our $SESSION_ID_HEADER_NAME = 'X-Transmission-Session-Id';
 my $JSON = JSON::MaybeXS->new;
 
